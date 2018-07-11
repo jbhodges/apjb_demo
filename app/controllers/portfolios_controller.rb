6a2ebf5fbbe32d3ad3f55acbe986dc8a075c1ecd
Blake Hodges <Blake@Blakes-iMac.attlocal.net>
2018-07-11 16:58:29 -0500

    Modify portfolios controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,5 +2,13 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+
+  end
+
 end
